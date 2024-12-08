9ce3a456ee66b1594b03360f3ab52b6fc7298ca3
fateme-shabani <shabanifateme22@gmail.com>
2024-12-08 18:06:46 +0330

    change event

--- practice2.js
+++ practice2.js
@@ -1,14 +1,17 @@
 const input = document.querySelector("input");
-input.addEventListener("click", (e) => {
+input.addEventListener("input", (e) => {
   setTimeout(() => {
     const value = e.target.value;
     const length = value.length;
     const p = document.querySelector("p");
+    p.innerText = length;
+
     if (length > 30) {
       p.style.color = "red";
-      p.innerText = length;
+      input.style.borderColor  = "red";
     } else {
-      p.innerText = length;
+        p.style.color = "green";
+        input.style.borderColor  = "green";
     }
-  }, 5000);
+  }, 500);
 });
